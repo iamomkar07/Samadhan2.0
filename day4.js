fedb4f473c60757c3f0ee3994d22708fdeabfb55
omkar kumar saini <omkarkumarsaini07@gmail.com>
2025-08-27 21:38:55 +0530

    Add files via upload

---
+++ day4.js
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>api returning</title>
+</head>
+<body>
+  <script>
+
+    
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello, World!"); 
+});
+const PORT = 3000; // server port
+app.listen(PORT, () => {
+  console.log(` Server running at : http://localhost:${PORT}`);
+});
+
+  </script>
+</body>
+</html>
